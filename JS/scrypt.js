bacdbd44950b547e0b84987ab859ffc17364d64b
Viktor898 <92379565+Viktor898@users.noreply.github.com>
2021-10-28 20:54:29 +0300

    dz_js

---
+++ JS/scrypt.js
@@ -0,0 +1,38 @@
+let city = 'Київ';
+city = 'Львів';
+let address = city;
+alert(address);
+
+let n = 25;
+let s = 3;
+let result = n**s;
+console.log(result);
+
+let pen = 4;
+let pen1 = ' Ручки по ';
+let pen2 = 5.25;
+let pen3 = ' грн';
+let pen4 = ' = ';
+let pen5 = pen * pen2;
+let pen6 =  ' грн, ';
+let pen7 = pen + pen1 + pen2 + pen3 + pen4 + pen5 + pen6;
+document.write(pen7);
+ 
+let ta = '   та   ';
+document.write(ta);
+
+let pensil = 6;
+let pensil1 = ' Олівців по ';
+let pensil2 = 3.50;
+let pensil3 = ' грн ';
+let pensil4 = ' = ';
+let pensil5 = pensil * pensil2;
+let pensil6 = ' грн.';
+let pensil7 = pensil + pensil1 + pensil2 + pensil3 + pensil4 + pensil5 + pensil6;
+document.write(pensil7)
+
+let vartist = pen5 + pensil5;
+let vartist1 = ' грн '
+let vartist2 = ' Загальна вартість = ' + vartist + vartist1;
+
+document.write(vartist2);
